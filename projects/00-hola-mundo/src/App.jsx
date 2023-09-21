f838b5a54365c2820156a823c3bba7c0414eef8b
LeandroDBechara <leandrobechara@alu.frt.utn.edu.ar>
2023-09-21 11:27:50 -0300

    a

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -10,7 +10,7 @@ export function App(){
             </header>
             <aside>
                 <button>
-                    Seguir
+                    Seguir ahora
                 </button>
             </aside>
         </article>
