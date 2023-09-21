e6814e99ac707c36e6c042c3745e81834f22bffd
LeandroDBechara <leandrobechara@alu.frt.utn.edu.ar>
2023-09-20 23:13:19 -0300

    Update main.jsx

--- projects/00-hola-mundo/src/main.jsx
+++ projects/00-hola-mundo/src/main.jsx
@@ -5,6 +5,7 @@ import ReactDOM from 'react-dom/client'
 ReactDOM.createRoot(document.getElementById('root')).render(
 <React.Fragment> 
   <button>"hola button"</button>
+  <button>"hola button"</button>
 </React.Fragment>
 
 )
