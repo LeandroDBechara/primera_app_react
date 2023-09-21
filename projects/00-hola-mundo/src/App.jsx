fee1db993a1c3c136b6b74c51ae3d0a95a399952
LeandroDBechara <leandrobechara@alu.frt.utn.edu.ar>
2023-09-21 11:26:33 -0300

    comit iniciar

---
+++ projects/00-hola-mundo/src/App.jsx
@@ -0,0 +1,18 @@
+export function App(){
+    return(
+        <article>
+            <header>
+                <img alt= "soy yo" src="https://pbs.twimg.com/profile_images/1270574183836200963/Yo7OusMX_400x400.jpg"></img>
+                <div>
+                    <strong>Leandro David Bechara</strong>
+                    <span>@corteyvieja</span>
+                </div>
+            </header>
+            <aside>
+                <button>
+                    Seguir
+                </button>
+            </aside>
+        </article>
+    )
+}
